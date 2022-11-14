560e09cc4349f2251b39d4e942a0c38b277dc5dd
Boris Shingarov <shingarov@labware.com>
2022-11-14 18:20:12 -0500

    Manually merge empty-and

--- src/Language/Fixpoint/Horn/Parse.hs
+++ src/Language/Fixpoint/Horn/Parse.hs
@@ -64,7 +64,7 @@ hCstrP :: Parser (H.Cstr ())
 -------------------------------------------------------------------------------
 hCstrP = parens body
   where
-    body =  H.CAnd  <$> (reserved "and"    *> some hCstrP)
+    body =  H.CAnd  <$> (reserved "and"    *> many hCstrP)
         <|> H.All   <$> (reserved "forall" *> hBindP)       <*> hCstrP
         <|> H.Any   <$> (reserved "exists" *> hBindP)       <*> hCstrP
         <|> H.Head  <$> hPredP                              <*> pure ()
